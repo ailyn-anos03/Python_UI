7fcb5bcf8019aef43a6a5f1b8e059ce8f4698df8
Ailyn Anos <ailyncanilloanos03@gmail.com>
2025-04-21 20:52:03 +0800

    Add files via upload

---
+++ Write_Files.py
@@ -0,0 +1,9 @@
+file = open("example.txt", "w")  
+
+file.write("Hello, this is a file handling example.\n")
+
+file.write("Python makes file handling easy!")
+
+file.close()  
+
+print("File written successfully!")
---
+++ pythonFile_IO.py
@@ -0,0 +1,8 @@
+from openpyxl import workbook, load_workbook
+
+wb = load_workbook('Grade.xlsx')
+
+
+
+
+
